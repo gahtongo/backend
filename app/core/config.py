7c282883041a94c63c02dd483d569a185f4e6ccc
gahtongo <beraccahjosh@gmail.com>
2026-04-12 20:24:00 +0100

    pw

--- app/core/config.py
+++ app/core/config.py
@@ -13,9 +13,9 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
 
-    DATABASE_URL: str = "sqlite:///./gahto.db"
+    #DATABASE_URL: str = "sqlite:///./gahto.db"
 
-    BACKEND_CORS_ORIGINS: str = "http://localhost:3000,http://127.0.0.1:3000"
+    BACKEND_CORS_ORIGINS: str = "http://localhost:3000,http://127.0.0.1:3000,https://gahto.org,https://www.gahto.org,https://ghatosite.vercel.app"
 
     FIRST_SUPERADMIN_EMAIL: str = "admin@gahto.org"
     FIRST_SUPERADMIN_PASSWORD: str = "@Admingahto1"
